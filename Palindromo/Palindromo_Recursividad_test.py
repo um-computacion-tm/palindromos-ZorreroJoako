821fbbe66c2aa81e249bfb9c0233976f842a049e
Joakito908 <j.zorrero@alumno.um.edu.ar>
2023-04-22 21:44:22 -0300

    TP Palindromo

---
+++ Palindromo/Palindromo_Recursividad_test.py
@@ -0,0 +1,15 @@
+import unittest 
+from Palindromo_Recursividad import es_palindromo_recursivo
+
+class TestPalindromo(unittest.TestCase):
+    def test_palindromo1(self):
+        self.assertTrue(es_palindromo_recursivo("anita lava la tina"))
+
+    def test_palindromo2(self):
+        self.assertTrue(es_palindromo_recursivo("A man a plan a canal Panama"))
+
+    def test_palindromo3(self):
+        self.assertFalse(es_palindromo_recursivo("Hello, world!"))
+
+if __name__ == '__main__':
+    unittest.main()
